a159a02b9f4200b8a67622639f6bf52d9020f10b
sunil <sunilgpawar2003@gmail.com>
2026-05-28 10:39:12 +0530

    DSA-1--> Swapping optimized in Selection Sort [quite faster]

--- src/com/sort/SelectionSort.java
+++ src/com/sort/SelectionSort.java
@@ -10,9 +10,9 @@ public class SelectionSort {
         for (int i = 0; i < arr.length; i++){
             for (int j = i+1; j < arr.length; j++){
                 if (arr[j] < arr[i]){
-                    arr[i] = arr[i] ^ arr[j];
-                    arr[j] = arr[i] ^ arr[j];
-                    arr[i] = arr[i] ^ arr[j];
+                   int temp = arr[j];
+                   arr[j] = arr[i];
+                   arr[i] = temp;
                 }
             }
         }
