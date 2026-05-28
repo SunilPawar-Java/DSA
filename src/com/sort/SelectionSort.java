f98df2de379befc84a12104b16dab1a2c7ff5961
sunil <sunilgpawar2003@gmail.com>
2026-05-28 09:29:57 +0530

    DSA-1--> Selection Sort [done]

---
+++ src/com/sort/SelectionSort.java
@@ -0,0 +1,21 @@
+package com.sort;
+
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+public class SelectionSort {
+    public static void main(String []args) {
+        int []arr = {2, 4, 6, 8, 1};
+
+        for (int i = 0; i < arr.length; i++){
+            for (int j = i+1; j < arr.length; j++){
+                if (arr[j] < arr[i]){
+                    arr[i] = arr[i] ^ arr[j];
+                    arr[j] = arr[i] ^ arr[j];
+                    arr[i] = arr[i] ^ arr[j];
+                }
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+}
